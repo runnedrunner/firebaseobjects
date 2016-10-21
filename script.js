f24b1d988cc4d3209f4f18ba07b31f5f30aad462
runnedrunner <auszhou@nuevaschool.org>
2016-10-21 08:39:12 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ onNewListItem("product-list", function(product) {
   var nameHeader = $("<h1 class='product-name'>" + product.name + "</h1>")
   var image = $("<img class='product-image'>").attr("src", product.picture)  
   var price = $("<h1 class='product-price'> Price: $" + product.price + "</h1>")
-  $(".product-list-container").append(nameHeader, image, price);
+  var rating = $("<h1 class='product-price'> Rating" + product.rating + "</h1>")
+  var rating = $("<h1 class='product-price'> Rating" + product.rating + "</h1>")
+
+  $(".product-list-container").append(nameHeader, image, price, rating);
   console.log(product);
 })
